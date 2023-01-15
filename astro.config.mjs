f1ec60da3b94a0efff52ed6a8771f034cdce22f6
Southball <6523469+southball@users.noreply.github.com>
2023-01-15 14:43:11 +0900

    Add mermaid support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import remarkMermaid from "@southball/remark-mermaid";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [[remarkMermaid, { themes: ["dark", "neutral"] }]],
+  },
 });
